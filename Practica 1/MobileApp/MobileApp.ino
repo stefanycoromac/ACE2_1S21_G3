cd20ae17e1d0e757003dcb583d52adba566082e9
201800726 <57029729+201800726@users.noreply.github.com>
2021-02-24 03:26:37 -0600

    Correciones MobileApp :dizzy:

--- Practica 1/MobileApp/MobileApp.ino
+++ Practica 1/MobileApp/MobileApp.ino
@@ -1,14 +1,16 @@
+/*#include <SoftwareSerial.h>
+  SoftwareSerial hc06(18, 19);*/
 /* Temperatura */
 #define sensorTemperatura 0
 float entradaTemp, celsiusTemp;
-String temp = "°C";
+String temp;
 /* END Temperatura */
 
 /* Oxigeno y Latidos */
 String oxigeno;
-float oxigenoValor = 90;
+float oxigenoValor = 80;
 String latidos;
-float latidosValor = 80;
+float latidosValor = 75;
 /* END  Oxigeno y Latidos */
 
 /* Login */
@@ -19,16 +21,15 @@ boolean logueado = false;
 
 void setup() {
   Serial.begin(9600);
+  //hc06.begin(9600);
   pinMode(loginLed, OUTPUT);
 }
 
 void loop() {
   verificarLogin();
-
-  while (logueado) {
+  if (logueado) {
     medirTemperatura();
     medirOxigenoPulso();
-    verificarLogin();
   }
 }
 
@@ -36,19 +37,19 @@ void medirTemperatura()
 {
   entradaTemp = analogRead(sensorTemperatura);
   celsiusTemp = (5 * entradaTemp * 100) / 1024;
-  temp = (String) celsiusTemp + " °C";
-  Serial.println(temp);
-  delay(100);
+  temp = "T;" + (String) celsiusTemp;
+  enviarDatos(temp);
+  delay(1000);
 }
 
 void medirOxigenoPulso() {
-  latidosValor = latidosValor +1;
+  latidosValor = latidosValor + 1;
   latidos = "L;" + (String) latidosValor;
-  Serial.println(latidos);
+  enviarDatos(latidos);
   delay(500);
-  oxigenoValor = oxigenoValor + 2;
+  oxigenoValor = oxigenoValor + 1;
   oxigeno = "O;" + (String) oxigenoValor;
-  Serial.println(oxigeno);
+  enviarDatos(oxigeno);
   delay(500);
 }
 
@@ -62,4 +63,11 @@ void verificarLogin() {
     logueado = false;
     digitalWrite(loginLed, LOW);
   }
+}
+
+void enviarDatos(String cadena) {
+  Serial.print(cadena);
+  /*for (char x : cadena) {
+    Serial.println(x);
+    }*/
 }

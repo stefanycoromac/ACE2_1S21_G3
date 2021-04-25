eb5b64d9e43e4eff71dbc41505148976cc952d81
Jessica Botón <jessicaboton@gmail.com>
2021-04-25 01:37:44 -0600

    Update WebApp URL

--- Practica 2/WebApp/src/app/services/global.ts
+++ Practica 2/WebApp/src/app/services/global.ts
@@ -1,4 +1,5 @@
 export const Global = {
-  // url: 'http://104.154.115.31:3000'
-  url: 'http://192.168.0.13:3000'
+  //TVE-V3 (Virtual Machine in Google Cloud Platform)
+  url: 'http://104.154.115.31:3000'
+  
 };
